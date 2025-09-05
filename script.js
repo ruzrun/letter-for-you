6fec71e55b8b77746b3f4e3848918650cf6f87c7
ruzrun <warunwan1314@gmail.com>
2025-09-06 01:21:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
 
 // prepare audio (place your file inside /audio/mySong.mp3)
 const audio = new Audio("audio/mySong.mp3");
@@ -32,5 +32,6 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
 
