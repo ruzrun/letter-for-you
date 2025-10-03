de7fb28929a495ffbb47b3d7804a6eaf1c150111
ruzrun <warunwan1314@gmail.com>
2025-10-03 12:16:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Letter messages
-const message = `Happy birthday, Damia,\n\nni first time warun wish damia birthday, warun nak jadikan this letter something yang special.\nDamia mungkin tahu yang warun suka Damia, dan itu memang betul.\nwarun nak bagitahu yang warun ada feeling dekat damia sejak form 2 lagi, tau tau sekarang dah form 4.\nSelama tu, perasaan ini banyak ajar warun... tentang sabar, tentang menghargai seseorang, tentang usaha untuk menjadi lebih baik setiap hari and banyak lagi.\nwalau kita tak pernah sembang, damia tetap seseorang yang jadi inspiration untuk warun.\nCara damia jaga diri, cara damia serius bila buat sesuatu, cara damia bimbing orang lain and banyak lagi, semua ni buat warun kagum dengan damia.\n\nWarun wish tahun ni damia dapat straight A kat UASA, dapat kawan kawan yang baik dan hari hari yang buat damia selalu happy. warun harap setiap hari damia rasa dihargai, rasa tenang and selalu bangga dengan diri sendiri sebab damia memang layak dapat semua tu.\n\nwarun tulis surat ni bukan untuk buat damia tak selesa ke apa, warun cuma nak luah kan perasaan warun yang warun simpan selama ni, walaupun sejak form 2, rasa macam kejap je masa berlalu and warun tulis surat ni untuk lepaskan perasaan ni slow slow. walaupun susah, taoi at least warun jujur dengan damia apa yang warun rasa. Satu hari nanti, mungkin semua ni cuma kenangan untuk warun, walaupun cuma kenangan, warun tahu satu hari nanti bila warun ingat balik, warun akan senyum, bukan sebab sedih, tapi sebab bersyukur pernah rasa semua ni.\n\nTerima kasih Damia, sebab pernah jadi seseorang yang special dalam hidup warun, walaupun hanya dari jauh.\n\nHappy Birthday Damia. May Allah always guide you to be a better person.\n
+const message = `Happy birthday, Damia.\n\nni first time warun wish damia birthday, warun nak jadikan this letter something yang special.\nDamia mungkin tahu yang warun suka Damia, dan itu memang betul.\nwarun nak bagitahu yang warun ada feeling dekat damia sejak form 2 lagi, tau tau sekarang dah form 4.\nSelama tu, perasaan ini banyak ajar warun... tentang sabar, tentang menghargai seseorang, tentang usaha untuk menjadi lebih baik setiap hari and banyak lagi.\nwalau kita tak pernah sembang, damia tetap seseorang yang jadi inspiration untuk warun.\nCara damia jaga diri, cara damia serius bila buat sesuatu, cara damia bimbing orang lain and banyak lagi, semua ni buat warun kagum dengan damia.\n\nWarun wish tahun ni damia dapat straight A kat UASA, dapat kawan kawan yang baik dan hari hari yang buat damia selalu happy. warun harap setiap hari damia rasa dihargai, rasa tenang and selalu bangga dengan diri sendiri sebab damia memang layak dapat semua tu.\n\nwarun tulis surat ni bukan untuk buat damia tak selesa ke apa, warun cuma nak luah kan perasaan warun yang warun simpan selama ni, walaupun sejak form 2, rasa macam kejap je masa berlalu and warun tulis surat ni untuk lepaskan perasaan ni slow slow. walaupun susah, tapi at least warun jujur dengan damia apa yang warun rasa. Satu hari nanti, mungkin semua ni cuma kenangan untuk warun, walaupun cuma kenangan, warun tahu satu hari nanti bila warun ingat balik, warun akan senyum, bukan sebab sedih, tapi sebab bersyukur pernah rasa semua ni.\n\nTerima kasih Damia, sebab pernah jadi seseorang yang special dalam hidup warun, walaupun hanya dari jauh.\n\nHappy Birthday Damia. May Allah always guide you to be a better person.\n
 `;
 
 // Prepare audio 
@@ -20,20 +20,20 @@ async function getServerTime() {
   try {
     const response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kuala_Lumpur");
     const data = await response.json();
-    return new Date(data.datetime); // masa server
+    return new Date(data.datetime); // masa server (global)
   } catch (e) {
     console.error("Gagal dapat masa server, fallback ke masa local.");
     return new Date(); // fallback kalau API gagal
   }
 }
 
-// Function ini minta masa dari server (worldtimeapi.org)
-// Jadi kalau orang tukar masa phone/laptop - tak boleh nak tipu
-// Kalau API gagal loading - fallback ke masa local device
+// Function ini hanya untuk masa dari server (worldtimeapi.org)
+// Kalau orang tukar masa kat phone/laptop - tak boleh nak tipu
+// Kalau API tak boleh loading - fallback ke masa local device
 
 // show letter
 async function showLetter() {
-  const now = await getServerTime(); // guna masa server
+  const now = await getServerTime(); // guna masa server 
 
   if (now >= allowedStart && now <= allowedEnd) {
     // kalau sekarang dalam range (antara 00:00 – 23:59 tarikh tu)
@@ -51,7 +51,7 @@ async function showLetter() {
     let i = 0;
     audio.play(); // start lagu
 
-    // Effect typewriter
+    // Effect typeWriter
     function typeWriter() {
       if (i < message.length) {
         typedText.innerHTML +=
@@ -60,11 +60,11 @@ async function showLetter() {
         // Kalau huruf biasa - terus tambah
 
         i++;
-        setTimeout(typeWriter, 60); // delay 60ms
+        setTimeout(typeWriter, 70); // delay 70ms - untuk tulisan 
       }
     }
 
-    typeWriter(); // start typeWriter
+    typeWriter(); // typeWriter start
 
   } else if (now < allowedStart) {
     alert("Birthday Damia tak sampai lagi... tunggu 8 Oktober boleh?");
@@ -75,6 +75,7 @@ async function showLetter() {
   }
 }
 
+//Thanks you Damia... You will always be my favourite incomplete wish
 
 
 
